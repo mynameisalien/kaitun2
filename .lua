372085ce0e052877ec99728c8db70cfd6b85e285
goodatmath <nbach1501@gmail.com>
2024-09-28 18:37:12 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,66 @@
+_G.CDK = { 
+    ['CDK Super Fast'] = true, 
+    ['Buy Color Haki'] = 3 
+}
+
+_G.AutoQuest = { 
+    ['RGB Aura Haki'] = true,
+    ['Pull Lever'] = true,
+    ['Kill Boss'] = true, 
+    ['Quest Race V4'] = true,
+    ['Quest Dough Awaken'] = true
+}
+
+_G.AutoRace = { 
+    ['Select Race'] = {'Human','Fishman','Skypiea','Mink'},
+    ['Lock Race'] = true,
+    ['Evo Race V3'] = true
+}
+
+_G.AutoMelee = { 
+    ['Godhuman'] = true
+}
+
+_G.AutoSword = { 
+    ['Saber'] = true,
+    ['Midnight Blade'] = true,
+    ['Shisui'] = true,
+    ['Saddi'] = true,
+    ['Wando'] = true,
+    ['Yama'] = true,
+    ['Koko'] = false,
+    ['Rengoku'] = true,
+    ['Canvander'] = true,
+    ['Buddy Sword'] = true,
+    ['Twin Hooks'] = true,
+    ['SpikeyTrident'] = true,
+    ['Hallow Scryte'] = true,
+    ['Dark Dagger'] = true,
+    ['Tushita'] = true,
+    ['True Triple Katana'] = false,
+    ['Cursed Dual Katana'] = true,
+    ['Shark Anchor'] = true
+}
+
+_G.AutoGun = {  
+    ['Kabucha'] = true,
+    ['Acidum Rifle'] = true,
+    ['Soul Guitar'] = true, 
+    ['Serpent Bow'] = true
+}
+
+_G.AutoFruit = { 
+    ['Main Fruit'] = {'Spider-Spider'},
+    ['Select Fruit'] = {'Quake-Quake','Flame-Flame','Ice-Ice','Sand-Sand','Dark-Dark','Light-Light','Magma-Magma'}
+}
+
+_G.AutoMastery = { 
+    ['Melee'] = true,
+    ['Sword'] = true,
+    ['Fruit'] = true
+}
+
+_G.Setting = {
+    ['FPS Booster'] = true,
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bestluxca/CrackedFree/main/RobloXploits%20-%20KaitunBF"))()
